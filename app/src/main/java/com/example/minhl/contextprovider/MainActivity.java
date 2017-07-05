b48f27db32d560b86638648321a676fed785d156
MinhSacred <minhlevan104@gmail.com>
2017-07-06 02:25:29 +0700

    New Change

--- app/src/main/java/com/example/minhl/contextprovider/MainActivity.java
+++ app/src/main/java/com/example/minhl/contextprovider/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.minhl.contextprovider;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends RuntimePermissionsActivity {
     private final static String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
     private final static Uri URI_CONTENT = ContactsContract.Contacts.CONTENT_URI;
     private final static String _ID = ContactsContract.Contacts._ID;
@@ -21,23 +30,22 @@ public class MainActivity extends AppCompatActivity {
     private final static Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
     private final static String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
     private final static String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+    private static final int REQUEST_PERMISSIONS = 100;
 
-    Cursor cursor;
+    private Cursor cursor;
     private ListView lvContact;
-    public List<String> listContact;
+    private List<String> listContact;
+    private Button btnShowContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initALotOfThings();
 
-        lvContact = (ListView) findViewById(R.id.lv);
-        listContact = new ArrayList<>();
-        getContact();
     }
 
     public void getContact() {
-
         cursor = getContentResolver().query(URI_CONTENT, null, null, null, null);
         if (cursor == null) {
             return;
@@ -48,17 +56,16 @@ public class MainActivity extends AppCompatActivity {
         StringBuffer element;
         while (cursor.moveToNext()) {
             element = new StringBuffer();
-            String contactID = cursor.getString(cursor.getColumnIndex(_ID));
-            String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
-            int hasphoneNumber = Integer.parseInt(cursor.getString
-                    (cursor.getColumnIndex(HAS_PHONE_NUMBER)));
-            if (isAvailable(hasphoneNumber)) {
-                element.append(contactID).append(". First Name: ").append(name);
-
-                Cursor phoneCursor = getContentResolver().query(PhoneCONTENT_URI, null,
-                        Phone_CONTACT_ID + " = ?", new String[]{contactID}, null); //vẫn chưa hiểu làm câu lệnh query này
-                while (phoneCursor.moveToNext()) {
-                    String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+            String contactID = getCussorStringWithColum(cursor, _ID);
+            String name = getCussorStringWithColum(cursor, DISPLAY_NAME);
+            int hasphoneNumber = Integer.parseInt(getCussorStringWithColum(cursor,
+                            HAS_PHONE_NUMBER));
+                    if (isAvailable(hasphoneNumber)) {
+                        element.append(contactID).append(". First Name: ").append(name);
+                        Cursor phoneCursor = getContentResolver().query(PhoneCONTENT_URI, null,
+                                Phone_CONTACT_ID + " = ?", new String[]{contactID}, null); //vẫn chưa hiểu làm câu lệnh query này
+                        while (phoneCursor.moveToNext()) {
+                            String phoneNumber = getCussorStringWithColum(phoneCursor, NUMBER);
                     element.append("\n    Phone number: " + phoneNumber);
                 }
                 phoneCursor.close();
@@ -69,10 +76,34 @@ public class MainActivity extends AppCompatActivity {
         setAdapter();
     }
 
+    public String getCussorStringWithColum(Cursor cursor, String index) {
+        return cursor.getString(cursor.getColumnIndex(index));
+    }
+
+    private void initALotOfThings() {
+        btnShowContact = (Button) findViewById(R.id.btn_show_contact);
+        lvContact = (ListView) findViewById(R.id.lv);
+        listContact = new ArrayList<>();
+        MainActivity.super.requestAppPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                R.string.runtime_permissions_txt, REQUEST_PERMISSIONS);
+
+        btnShowContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getContact();
+            }
+        });
+    }
+
     public void setAdapter() {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, listContact);
         lvContact.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode) {
+        Toast.makeText(this, "Ahihi đc chấp nhận r, bấm Button đi", Toast.LENGTH_SHORT).show();
     }
 
     public boolean isAvailable(int phoneNumber) {
